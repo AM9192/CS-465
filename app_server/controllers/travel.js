1dfc23d174af41a85869810a81707f2c76442064
August Moews <august.moews@snhu.edu>
2025-09-27 13:45:12 -0500

    Module 3 Completed Baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,4 +1,13 @@
-// Controller for the Travel page
+// app_server/controllers/travel.js
+const fs = require('fs');
+const path = require('path');
+
 exports.travelList = (req, res) => {
-  res.render('travel', { title: 'Travlr Getaways' });
+  const dataFile = path.join(__dirname, '..', '..', 'data', 'trips.json');
+  const trips = JSON.parse(fs.readFileSync(dataFile, 'utf8'));
+
+  res.render('travel', {
+    title: 'Travlr Getaways',
+    trips
+  });
 };
